d42b04bb665045e2df6f4d26694bdc57dcb0e657
sebastien <sebaserracyahoo.com>
2021-02-10 20:47:03 +0100

    implementing sass

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts/layout')
 @section('content')
-        <section>
-                <div>Pizza List</div>
+        <div class="wrapper pizza-index">
+                <div>Pizza Orders</div>
                     @foreach($pizzas as $pizza)
-                         <div>{{ $pizza->name }} - {{ $pizza->type }} - {{ $pizza->base }}</div>
+                        <div class="pizza-item">
+                                <img src="/img/pizza.png" alt="pizza-icon">
+                                <h4>
+                                 <a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a>
+                                </h4>
+                        </div>
                     @endforeach
                 </div>
-        </section>
+        </div>
 @endsection
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
         <section>
         <div class="restaurant-name">Pizza House</div>
-        <img src="/img/pizza.jpg" alt="pizza">
+        <img src="/img/pizza.jpg" alt="pizza" class="head">
         <div class="title">The best pizzas ever !</div>
         <p class="mssg">{{ session('mssg') }}</p>
         <a href="/pizzas/create">Order a Pizza</a>

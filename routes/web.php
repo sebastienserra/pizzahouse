b0e327a7b636c0869b63bf5596e79203e42090ec
sebastien <sebaserracyahoo.com>
2021-02-11 09:52:49 +0100

    Protecting Routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,11 @@ use App\Http\Controllers\PizzaController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/pizzas', [PizzaController::class, 'index']);
+Route::get('/pizzas', [PizzaController::class, 'index'])->middleware('auth');
 Route::get('/pizzas/create', [PizzaController::class, 'create']);
 Route::post('pizzas', [PizzaController::class, 'store' ]);
-Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
-Route::delete('pizzas/{id}', [PizzaController::class, 'destroy']);
+Route::get('/pizzas/{id}', [PizzaController::class, 'show'])->middleware('auth');
+Route::delete('pizzas/{id}', [PizzaController::class, 'destroy'])->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
